1cc9c01c53c221cda1cf7cb524080466e8e21559
Northon T. Cavazzoni <northoncavazzoni@hotmail.com>
2020-12-06 18:56:12 -0300

    Criacao da impressao das cidades e do melhor path.

--- UIAlgoritmoGenetico/Forms/Caixeiro.Designer.cs
+++ UIAlgoritmoGenetico/Forms/Caixeiro.Designer.cs
@@ -28,34 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnIniciarSetup = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPopulation = new System.Windows.Forms.Label();
+            this.lblBest = new System.Windows.Forms.Label();
+            this.lblBestScoreOfPopulation = new System.Windows.Forms.Label();
+            this.lblBestScoreEver = new System.Windows.Forms.Label();
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnIniciarSetup
             // 
-            this.button1.Location = new System.Drawing.Point(855, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnIniciarSetup.Location = new System.Drawing.Point(1208, 24);
+            this.btnIniciarSetup.Name = "btnIniciarSetup";
+            this.btnIniciarSetup.Size = new System.Drawing.Size(100, 23);
+            this.btnIniciarSetup.TabIndex = 0;
+            this.btnIniciarSetup.Text = "Setup";
+            this.btnIniciarSetup.UseVisualStyleBackColor = true;
+            this.btnIniciarSetup.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(841, 64);
+            this.textBox1.Location = new System.Drawing.Point(1236, 53);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(45, 22);
             this.textBox1.TabIndex = 1;
+            // 
+            // lblPopulation
+            // 
+            this.lblPopulation.AutoSize = true;
+            this.lblPopulation.Location = new System.Drawing.Point(70, 13);
+            this.lblPopulation.Name = "lblPopulation";
+            this.lblPopulation.Size = new System.Drawing.Size(75, 17);
+            this.lblPopulation.TabIndex = 2;
+            this.lblPopulation.Text = "Population";
+            // 
+            // lblBest
+            // 
+            this.lblBest.AutoSize = true;
+            this.lblBest.Location = new System.Drawing.Point(927, 13);
+            this.lblBest.Name = "lblBest";
+            this.lblBest.Size = new System.Drawing.Size(36, 17);
+            this.lblBest.TabIndex = 3;
+            this.lblBest.Text = "Best";
+            // 
+            // lblBestScoreOfPopulation
+            // 
+            this.lblBestScoreOfPopulation.AutoSize = true;
+            this.lblBestScoreOfPopulation.Location = new System.Drawing.Point(198, 13);
+            this.lblBestScoreOfPopulation.Name = "lblBestScoreOfPopulation";
+            this.lblBestScoreOfPopulation.Size = new System.Drawing.Size(123, 17);
+            this.lblBestScoreOfPopulation.TabIndex = 4;
+            this.lblBestScoreOfPopulation.Text = "Best of Population";
+            // 
+            // lblBestScoreEver
+            // 
+            this.lblBestScoreEver.AutoSize = true;
+            this.lblBestScoreEver.Location = new System.Drawing.Point(1001, 13);
+            this.lblBestScoreEver.Name = "lblBestScoreEver";
+            this.lblBestScoreEver.Size = new System.Drawing.Size(123, 17);
+            this.lblBestScoreEver.TabIndex = 5;
+            this.lblBestScoreEver.Text = "Best of Population";
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(716, 13);
+            this.btnEvaluate.Name = "btnMutate";
+            this.btnEvaluate.Size = new System.Drawing.Size(105, 23);
+            this.btnEvaluate.TabIndex = 6;
+            this.btnEvaluate.Text = "Start Mutate";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnMutate_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Swap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Caixeiro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 626);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1329, 778);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.lblBestScoreEver);
+            this.Controls.Add(this.lblBestScoreOfPopulation);
+            this.Controls.Add(this.lblBest);
+            this.Controls.Add(this.lblPopulation);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnIniciarSetup);
             this.Name = "Caixeiro";
             this.Text = "Caixeiro";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Caixeiro_FormClosed);
@@ -67,7 +135,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnIniciarSetup;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPopulation;
+        private System.Windows.Forms.Label lblBest;
+        private System.Windows.Forms.Label lblBestScoreOfPopulation;
+        private System.Windows.Forms.Label lblBestScoreEver;
+        private System.Windows.Forms.Button btnEvaluate;
+        private System.Windows.Forms.Button button1;
     }
 }
--- UIAlgoritmoGenetico/Forms/Caixeiro.cs
+++ UIAlgoritmoGenetico/Forms/Caixeiro.cs
@@ -7,24 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UIAlgoritmoGenetico.Classes;
 
 namespace UIAlgoritmoGenetico.Forms
 {
     public partial class Caixeiro : Form
     {
         FormHome formHome;
-        List<int[]> cities;
+        List<City> cities;
+        Random random = new Random();
+        Brush aBrush = Brushes.Black;
+        Graphics g;
+        int xSquarePopulation = 50;
+        int ySquarePopulation = 50;
+        int squaresWidth = 400;
+        int squaresHeight = 400;
+        int xSquareBest = 600;
+        int ySquareBest = 50;
+        List<City> path;
+
 
         public Caixeiro(FormHome formHome)
         {
             InitializeComponent();
             this.formHome = formHome;
-            cities = new List<int[]>();
+            cities = new List<City>();
+            xSquareBest = xSquarePopulation + squaresWidth + 50;
+            path = new List<City>();
+            g = this.CreateGraphics();
+
         }
 
         private void Caixeiro_Load(object sender, EventArgs e)
         {
-
         }
 
         private void Caixeiro_FormClosed(object sender, FormClosedEventArgs e)
@@ -35,33 +50,132 @@ namespace UIAlgoritmoGenetico.Forms
         private void button1_Click(object sender, EventArgs e)
         {
             cities.Clear();
+            path.Clear();
 
-            int quantidadeDeCidades = int.Parse(textBox1.Text);
-            Random random = new Random();
-            
-            for (int i =0; i< quantidadeDeCidades; i++)
+            if (!int.TryParse(textBox1.Text, out int quantidadeDeCidades))
             {
-
-                int randomX = random.Next(0, this.Width);
-                int randomY = random.Next(0, this.Height);
-
-
-                int[] city;
-                city = new int[2];
-                city[0] = randomX;
-                city[1] = randomY;
-
-                cities.Add(city);
+                MessageBox.Show("O número de pontos deve ser informado!");
+                return;
             }
 
-            Brush aBrush = Brushes.Black;
-            Graphics g = this.CreateGraphics();
+            for (int i = 0; i < quantidadeDeCidades; i++)
+            {
+                int randomX = random.Next(xSquarePopulation + 10, squaresWidth + xSquarePopulation - 10);
+                int randomY = random.Next(xSquarePopulation + 10, squaresWidth + xSquarePopulation - 10);
+
+                City city = new City();
+                city.x = randomX;
+                city.y = randomY;
+
+                cities.Add(city);
+                path.Add(city);
+            }
+            PrintScreen();
+        }
+
+        private double calculatePathDistance(List<City> path)
+        {
+            double distance = 0;
+            for (int i = 1; i < path.Count; i++)
+            {
+                distance += CalculateDistance(path[i - 1], path[i]);
+            }
+            return distance;
+        }
+
+        private double CalculateDistance(City city1, City city2)
+        {
+            return (double)Math.Sqrt(Math.Pow(city2.x - city1.x, 2) + Math.Pow(city2.y - city1.y, 2));
+        }
+
+        private void PrintScreen()
+        {
+            g.Clear(DefaultBackColor);
+            DrawPopulations();
+            DrawBest();
+            lblBestScoreOfPopulation.Text = calculatePathDistance(cities).ToString("F2");
+            lblBestScoreEver.Text = calculatePathDistance(path).ToString("F2");
+        }
+
+
+        private void DrawPopulations()
+        {
+            g.DrawRectangle(new Pen(aBrush), xSquarePopulation, ySquarePopulation, squaresWidth, squaresHeight);
 
             for (int i = 0; i < cities.Count; i++)
             {
-                g.FillRectangle(aBrush, cities[i][0], cities[i][1], 5, 5);
+                g.FillEllipse(aBrush, cities[i].x - 3, cities[i].y - 3, 6, 6);
+            }
+
+            Pen linePen = new Pen(Brushes.Black, 1);
+
+            for (int i = 0; i < cities.Count - 1; i++)
+            {
+                Point pt1 = new Point(cities[i].x, cities[i].y);
+                Point pt2 = new Point(cities[i + 1].x, cities[i + 1].y);
+                g.DrawLine(linePen, pt1, pt2);
             }
         }
 
-      }
+        private void DrawBest()
+        {
+
+            g.DrawRectangle(new Pen(aBrush), xSquareBest, ySquareBest, squaresWidth, squaresHeight);
+            for (int i = 0; i < path.Count; i++)
+            {
+                int xShifted = (path[i].x - xSquarePopulation) + xSquareBest;
+                g.FillEllipse(aBrush, xShifted - 3, path[i].y - 3, 6, 6);
+            }
+
+            Pen linePen = new Pen(Brushes.Black, 1);
+
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                int xShifter = xSquareBest - xSquarePopulation;
+                Point pt1 = new Point(path[i].x + xShifter, path[i].y);
+                Point pt2 = new Point(path[i + 1].x + xShifter, path[i + 1].y);
+                g.DrawLine(linePen, pt1, pt2);
+            }
+
+        }
+
+        private void btnMutate_Click(object sender, EventArgs e)
+        {
+            int tries = 0;
+            while (tries < 1000)
+            {
+                SwapOrder();
+                CheckNewBest();
+                PrintScreen();
+                tries++;
+            }
+        }
+
+        private void SwapOrder()
+        {
+            int iCity1 = random.Next(0, cities.Count - 1);
+            int iCity2 = random.Next(0, cities.Count - 1);
+
+            City tempCity = cities[iCity1];
+            cities[iCity1] = cities[iCity2];
+            cities[iCity2] = tempCity;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SwapOrder();
+            CheckNewBest();
+            PrintScreen();
+        }
+
+        private void CheckNewBest()
+        {
+            if (calculatePathDistance(cities) < calculatePathDistance(path))
+            {
+                City[] array = new City[cities.Count];
+                cities.CopyTo(array);
+                path = array.ToList();
+            }
+        }
+    }
 }
